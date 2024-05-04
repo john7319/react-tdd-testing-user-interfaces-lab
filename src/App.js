2e0d90817d62906b4a46a56f663d5c35b7a6a9fa
john7319 <jonniekaras@gmail.com>
2024-05-04 09:52:13 +0300

    Added an image link

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ function App() {
   <div>
     <h1>Hi, I'm John Karanja</h1>
     <>
-    <img src= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fhigh-resolution&psig=AOvVaw0ysyYdFBq0uuQ-TwVCfDBu&ust=1714888926754000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLCuyZ6p84UDFQAAAAAdAAAAABAE" alt="leaf-picture"/>
+    <img src= "https://media.istockphoto.com/id/1832240689/photo/autumn-leaves-in-lake.jpg?s=1024x1024&w=is&k=20&c=_USPQOrg8jU8JEEM3WDUFaK0ElyrQabPHPLZkjHynjI="/>
     <h1> About Me</h1>
     <p>This is my about</p>
     <a href="https://github.com/john7319/react-tdd-testing-user-interfaces-lab">Github</a>
     <br/>
-    
+
     <a href="https://www.linkedin.com/">Linkedin</a>
     </>
  </div>);
