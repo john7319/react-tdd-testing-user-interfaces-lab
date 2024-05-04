04180ed8e8a01426102bc561cd06c6ec0c2a42a4
john7319 <jonniekaras@gmail.com>
2024-05-04 09:47:46 +0300

    added tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm John Karanja</h1>
+    <>
+    <img src= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fhigh-resolution&psig=AOvVaw0ysyYdFBq0uuQ-TwVCfDBu&ust=1714888926754000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLCuyZ6p84UDFQAAAAAdAAAAABAE" alt="leaf-picture"/>
+    <h1> About Me</h1>
+    <p>This is my about</p>
+    <a href="https://github.com/john7319/react-tdd-testing-user-interfaces-lab">Github</a>
+    <br/>
+    
+    <a href="https://www.linkedin.com/">Linkedin</a>
+    </>
+ </div>);
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,62 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+  
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("Display an image with alt text 'leaf-picture'", () => {
+    render(<App />);
+    const imageAlt = screen.getByAltText("leaf-picture"); 
+    expect(imageAlt).toBeInTheDocument();
+});
+
+test("displays a second-level heading with the text `About me`", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const secondLevelHeading = screen.getByRole("heading", {
+      name: /About Me/i,
+      exact: false,
+      level: 1
+    });
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+});
+
+
+test("Render a paragraph with text 'This is my about'", () => {
+    render(<App />);
+    const paragraphText = screen.getByText("This is my about"); // Look for exact text content
+    expect(paragraphText).toBeInTheDocument();
+});
+
+test('renders github link', () => {
+  // "container" represents all the DOM elements rendered by your component
+  render(<App />)
+  const linkElement = screen.getByRole("link", {
+    name: /Github/i
+  })
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Linkedin link', () => {
+  // "container" represents all the DOM elements rendered by your component
+  render(<App />)
+  const linkElement1 = screen.getByRole("link", {
+    name: /Linkedin/i
+  })
+  expect(linkElement1).toBeInTheDocument();
+});
